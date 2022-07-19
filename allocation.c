116f30a3b704b71198840da8a56e74356ab90d93
erinhemsley <102770017+erinhemsley@users.noreply.github.com>
2022-07-19 20:08:17 +0000

    fixed file spelling

--- allocation.c
+++ allocation.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+things to do:
+--------------------
+memory alocation algs:
+-first fit
+-best fit
+-worst fit
+
+
+*/
 
 
 int main(int argv, char *arg[]){
